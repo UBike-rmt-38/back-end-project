608588ee5d263840b02b4b8c222fbb41503c8983
edoridha <edo.ridha@gmail.com>
2023-08-25 00:29:01 +0700

    feat: Created functions to generate an access token and verify an access token.

---
+++ postgres-app/helpers/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const JWT_KEY = process.env.JWT_KEY
+
+function signToken(user) {
+    const { id, email, username } = user
+    return jwt.sign({ id, email, username,}, JWT_KEY);
+}
+function verifyToken(access_token) {
+    return jwt.verify(access_token, JWT_KEY);
+}
+
+module.exports = { signToken, verifyToken}
